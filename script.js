82fd893ceccb4d830275fd6d598e0e8d0aa280d0
Abhishek Vashisth <118257746+Abhi-33@users.noreply.github.com>
2024-03-06 20:42:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+// //Download Resume 
+// let resume  = document.querySelector('.btn-2');
+// resume.addEventListener('click' , (e)=>{
+//     return e;
+// })
+
+//Firing Event on submit button
+// let submit = document.getElementById('btn-3');
+// submit.addEventListener('click', (event)=>{
+//     event.preventDefault(); //Prevent default form submission behaviour
+//     // location.reload();
+//     //show alert message
+//     alert('Form submitted Succesfully ✅');
+// })
+
+let submit = document.getElementById('btn-3');
+submit.addEventListener('click', (event) => {
+    // Show alert message
+    alert('Form submitted successfully ✅');
+    event.preventDefault(); // Prevent default form submission behavior
+    location.reload();    
+});
